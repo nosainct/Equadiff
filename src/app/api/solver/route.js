3ceb3b1b89e7851a6d3a6e9ee946f3752b0c6b6f
nosainct <90696536+nosainct@users.noreply.github.com>
2023-12-06 15:11:49 +0100

    Update route.js

--- src/app/api/solver/route.js
+++ src/app/api/solver/route.js
@@ -1,19 +1,25 @@
 import { PythonShell } from 'python-shell';
-export async function POST(request) {
-    const res = await request.json()
-    let options = {
-        mode: 'text',
-        pythonOptions: ['-u'],
-        scriptPath: 'C:/Users/noasa/Documents/BUT/Maths/Equa_Diff/Equadiff/src/app/api/solver/solver.py', // Ajustez le chemin selon l'organisation de votre projet
-        args: [JSON.stringify(request.body)]
-      };
-  
-      PythonShell.run('solver.py', options, (err, results) => {
-        if (err) {
-          console.error(err);
-          return Response.send('Internal Server Error');
-        } else {
-            return Response.json({ res })
-        }
-      });
-  }
+
+export async function POST(request, response) {
+    const requestData = await request.json();
+    console.log("args : ", JSON.stringify(requestData));
+    return new Promise((resolve, reject) => {
+        let options = {
+            mode: 'text',
+            pythonOptions: ['-u'],
+            scriptPath: 'C:/Users/noasa/Documents/BUT/Maths/Equa_Diff/Equadiff/src/app/api/solver', // Directory path
+            args: [JSON.stringify(requestData)]
+        };
+
+        PythonShell.run('solver.py', options, (err, results) => {
+            if (err) {
+                console.error(err);
+                response.status(500).send('Internal Server Error');
+                reject(err);  // Reject the promise on error
+            } else {
+                response.json({ results: results });
+                resolve();  // Resolve the promise on success
+            }
+        });
+    });
+}
